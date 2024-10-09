5b3c7c24086203a8702d6aa68aa6265c87669ff3
JhonQuintero <jfqm7710@gmail.com>
2024-10-09 09:48:43 -0500

    feat(settings): setup project with gradle and kotlin version

--- app/src/main/java/co/baco/baco/data/database/BacoDatabase.kt
+++ app/src/main/java/co/baco/baco/data/database/BacoDatabase.kt
@@ -7,7 +7,7 @@ import co.baco.baco.common.utils.Converters
 import co.baco.baco.data.database.dao.RegisterDao
 import co.baco.baco.data.database.entities.RegisterEntity
 
-@Database(entities = [RegisterEntity::class], version = 1)
+@Database(entities = [RegisterEntity::class], version = 1, exportSchema = false)
 @TypeConverters(Converters::class)
 abstract class BacoDatabase : RoomDatabase(){
 
